0eea3ec1f73d268429cba1f9211b794c53d04e23
Ana Beatris ❤️ <programadorsistemas0@gmail.com>
2025-09-04 10:50:53 -0300

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{float desc,vl_unitario,valor_total,vl_excel;
+
+printf ("digite o valor unitario%:");
+scanf ("%f",&vl_unitario)
+
+printf ("digite o valor de desconto em % ")
+scanf ("%f",8desc)
+desc = desc/100;
+
+vl_unitario_desc=vl_unitario-(vl_unitario *desc);
+
+
+printf ("digite a qde de botton vendida ");
+scanf("%f"&vl_unitario);
+
+if (qde<101)
+valor_total = 100*vl_unitario;
+
+else
+vl_exced = qde - 100;
+vl_total = 100*vl_unitario + vl_exced * vl_unitario_desc;
+
+
+
+
+
+
+
+}
+
+
